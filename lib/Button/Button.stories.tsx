9efc2ca8e9fabe8f57ddd3fb8d430e247553ef55
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2024-04-29 19:53:18 +0100

    export styles

--- lib/Button/Button.stories.tsx
+++ lib/Button/Button.stories.tsx
@@ -2,9 +2,31 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Button } from "./Button";
 
+
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Button> = {
   component: Button,
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: [
+        "default",
+        "secondary",
+        "destructive",
+        "ghost",
+        "link",
+        "outline",
+      ],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["default", "icon", "sm", "lg"],
+    },
+  },
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 };
 
 export default meta;
@@ -12,6 +34,7 @@ type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   args: {
-    children: 'Click me!'
+    children: 'Click me!',
+    variant: 'default'
   },
 };
--- lib/Button/Button.tsx
+++ lib/Button/Button.tsx
@@ -1,9 +1,52 @@
-import { ReactNode } from "react";
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface ButtonProps {
-  children: ReactNode;
+import { cn } from "../utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-2 bg-red bg-main border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none'",
+        outline: "",
+        ghost: "",
+        link: "",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
 }
 
-export const Button = ({ children }: ButtonProps): ReactNode => {
-  return <button className="bg-red-500">{children}</button>
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
